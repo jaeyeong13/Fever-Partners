085b52886c0858b705e76db6ac3b38e16af0fed8
Eric <yonghwa814@naver.com>
2024-02-01 05:08:44 +0900

    [feat]Goal Creation Page

--- config/urls.py
+++ config/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', include('apps.goal_management.urls')),
 ]
 
 # 개발 중에만 사용 (production에서는 웹 서버에서 처리하도록 하자)
