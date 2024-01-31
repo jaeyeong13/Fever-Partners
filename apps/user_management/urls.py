9075afce7e2c52596f7b747a86a861718b6837cc
shash0423 <shash042319@naver.com>
2024-01-31 13:49:32 +0900

    [feat]개인 페이지 추가(기본적인 정보만. 수정하기 버튼, 참여중인 그룹 추가 예정)

--- apps/user_management/urls.py
+++ apps/user_management/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import *
 
 app_name = 'user_management'
 
 urlpatterns = [
-
+    path('detail/<int:pk>', detail, name='detail'),
 ]
--- apps/user_management/views.py
+++ apps/user_management/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
+def detail(request, pk):
+    user = User.objects.get(id=pk)
+    ctx = {
+        'user':user
+    }
+    return render(request, 'user_management/user_detail.html', ctx)
