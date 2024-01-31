16f65c651e32c87f5904062b0fc6fc9beb69760c
minju <iaki111@naver.com>
2024-01-31 16:29:43 +0900

    [fix]:room_model

--- apps/rooms/models.py
+++ apps/rooms/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
-# Create your models here.
+from apps.user_management.models import User
+
+class Room(models.Model):
+    name = models.CharField(max_length=100)
+    master = models.ForeignKey(User, related_name='rooms_managed', on_delete=models.CASCADE)
+    members = models.ManyToManyField(User, related_name='rooms_joined')
+    tags = models.CharField(max_length=100)
+    rule = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
