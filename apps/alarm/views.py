9a2c4f5918d3888cbfefb7715185f1fbd6ac1933
shash0423 <shash042319@naver.com>
2024-02-01 14:38:38 +0900

    [fix] detail 창에서 알림 삭제가능

--- apps/alarm/views.py
+++ apps/alarm/views.py
@@ -4,4 +4,4 @@ from apps.alarm.models import Alarm
 def delete(request, pk):
     if request.method == 'POST':
         Alarm.objects.get(id=pk).delete()
-    return redirect('user_management:detail', pk=request.user.pk)
+    return redirect('user_management:detail')
