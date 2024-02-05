1dcb7c2afca1b7de2b789457b9a8e737c07f9b0d
jaeyeong13 <141209201+jaeyeong13@users.noreply.github.com>
2024-02-05 22:17:49 +0900

    [feat] 포스팅에 댓글 기능 추가

--- apps/free_board/models.py
+++ apps/free_board/models.py
@@ -4,7 +4,7 @@ from apps.user_management.models import User
 
 # Create your models here.
 class Post(models.Model):
-  title = models.CharField(max_length=200)
+  title = models.CharField(max_length=200, null=True)
   content = models.TextField()
   author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
   created_at = models.DateTimeField(auto_now_add=True)
@@ -19,6 +19,3 @@ class Comment(models.Model):
   author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
   content = models.TextField()
   created_at = models.DateTimeField(auto_now_add=True)
-
-  def __str__(self):
-    return f"Comment by {self.author.username} on post {self.post}"
--- apps/free_board/views.py
+++ apps/free_board/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def create_post(request):
-  return HttpResponse('이것은 커뮤니티 페이지')
+  post_list = Post.objects.order_by('-created_at')
+  context = {'post_list': post_list}
+  return render(request, 'free_board/board_list.html', context)
+
+def post_detail(request, post_id):
+  post = Post.objects.get(id=post_id)
+  context = {'post': post}
+  return render(request, 'free_board/post_detail.html', context)
+
+@login_required  # 댓글을 작성하려면 사용자가 로그인해야 합니다.
+def create_comment(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        if content:  # 댓글 내용이 있을 경우에만 생성
+            # Comment 인스턴스를 직접 생성하고 저장하는 방법으로 변경
+            comment = Comment(post=post, author=request.user, content=content, created_at=timezone.now())
+            comment.save()
+    return redirect('free_board:detail', post_id=post_id)
