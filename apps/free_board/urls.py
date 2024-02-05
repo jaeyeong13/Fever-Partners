440674f33cab9658b7480f70d636f52fb415263e
jaeyeong13 <141209201+jaeyeong13@users.noreply.github.com>
2024-02-05 19:55:18 +0900

    [feat] free_board에 Post와 Comment 모델 추가

--- apps/free_board/urls.py
+++ apps/free_board/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from . import views
 from .views import *
 
 app_name = 'free_board'
 
 urlpatterns = [
-  path('free_board/', create_post, name='create_post')
+  path('', create_post, name='create_post'),    # /board/ 접속 시 볼 수 있는 화면
 ]
