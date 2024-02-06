cd73ed98b2cc53d8d3e82f2a7416f8d75f74deb6
YonghwaJeong <yonghwa814@naver.com>
2024-02-06 23:45:34 +0900

    [feat]group_admin > member_list

--- apps/group_administration/urls.py
+++ apps/group_administration/urls.py
@@ -6,4 +6,5 @@ app_name = 'group_administration'
 
 urlpatterns = [
     path('main/<int:room_id>', show_admin_page, name='group_admin_main'),
+    path('member_list/<int:room_id>', show_member_list, name='member_list')
 ]
--- apps/group_administration/views.py
+++ apps/group_administration/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from apps.group_management.models import Room
 
 # 이 부분 나중에 수정되어야 함
 def show_admin_page(request, room_id):
-    return render(request, 'group_administration/group_admin_base.html')
+    ctx = {
+        'room_id':room_id,
+    }
+    return render(request, 'group_administration/group_admin_base.html', ctx)
+
+def show_member_list(request, room_id):
+    members = Room.objects.get(pk=room_id).members
+    cnt = {
+        'members':members,
+        'room_id':room_id,
+    }
+    return render(request, 'group_administration/group_member_list.html', cnt)
