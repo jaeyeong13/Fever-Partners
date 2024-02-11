1d5fac3a8a819b15c20eba697c232a2f2ab8e7d3
jaeyeong13 <devlog.jae13@gmail.com>
2024-02-11 20:48:26 +0900

    [fix] 나눈 게시판에서 '좋아요' 오류 해결

--- apps/free_board/views.py
+++ apps/free_board/views.py
@@ -138,10 +138,11 @@ def comment_delete(request, comment_id):
 
 
 @login_required(login_url='user_management:login')
-def vote_post(request, post_id):
+def vote_post(request, study_room_id, post_id):
     post = get_object_or_404(Post, pk=post_id)
     if request.user != post.author:
         messages.error(request, '본인이 작성한 글은 추천할 수 없습니다.')
     else:
         post.voter.add(request.user)
-    return redirect('free_board:detail', post_id=post.id)
+    return redirect('free_board:detail', study_room_id=study_room_id, post_id=post.id)
+
