86e49fcd751ea4ce1e0c3bffc737007dd3ec2840
jaeyeong13 <devlog.jae13@gmail.com>
2024-02-11 21:53:06 +0900

    [style] 불필요한 코드 삭제

--- apps/free_board/models.py
+++ apps/free_board/models.py
@@ -1,21 +1,18 @@
-from django.conf import settings
-from django.db import models
 from django.contrib.auth.models import User
-
 from django.conf import settings
 from django.db import models
-from apps.group_management.models import Room  # Room 모델을 임포트
+from apps.group_management.models import Room
 
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_post')
-    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='room_posts', null=True)  # Room 모델을 참조하는 필드 추가
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='room_posts', null=True)
     title = models.CharField(max_length=200, null=True)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True, blank=True)
     voter = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='voter_post')
-    notice = models.BooleanField(default=False)  # 공지글 여부 체크하기
+    notice = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
