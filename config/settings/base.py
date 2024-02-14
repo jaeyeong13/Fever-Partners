eb0070da7b7a46e67f665846b4a427c6f5c5f7ab
hbam1 <hbbaamm@gmail.com>
2024-02-14 14:11:56 +0900

    [feat] google, kakao 추가 및 user model new field 추가

--- config/settings/base.py
+++ config/settings/base.py
@@ -47,6 +47,8 @@ THIRD_PARTY_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.naver',
     'django_elasticsearch_dsl',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.kakao',
 ]
 
 ELASTICSEARCH_DSL = {
@@ -60,6 +62,9 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',    
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
+
+# allauth site_id
+SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/update/start'   # social login redirect
 ACCOUNT_LOGOUT_REDIRECT_URL = '/start'  # logout redirect
