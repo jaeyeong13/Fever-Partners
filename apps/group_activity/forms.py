1b3e6782d2c038a2ac6d010821888205aa05b615
shash0423 <shash042319@naver.com>
2024-02-15 15:09:33 +0900

    [merge] style/jaekwan <- fix/auth

--- apps/group_activity/forms.py
+++ apps/group_activity/forms.py
@@ -1,10 +1,20 @@
 from django import forms
 from .models import Authentication, MemberAuthentication
+import datetime
 
 class AuthenticationForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(AuthenticationForm, self).__init__(*args, **kwargs)
+        self.fields['start'].initial = datetime.datetime.now()
+        self.fields['end'].initial = datetime.datetime.now() + datetime.timedelta(hours=2)
+
     class Meta:
         model = Authentication
-        fields =['start', 'end']
+        fields = ['start', 'end']
+        widgets = {
+            'start': forms.TextInput(attrs={'class': 'flatpickr'}),
+            'end': forms.TextInput(attrs={'class': 'flatpickr'}),
+        }
 
 class MemberAuthenticationForm(forms.ModelForm):
     class Meta:
--- apps/group_activity/urls.py
+++ apps/group_activity/urls.py
@@ -5,14 +5,15 @@ app_name = 'group_activity'
 
 urlpatterns = [
     path('main/<int:room_id>', show_activity_main, name="main_page"),
-    path('activate/<int:pk>/', activate, name='activate'),
-    path('authentication/<int:pk>', create_authentication, name='create_authentication'),
-    path('auth/<int:pk>', create_auth, name='create_auth'),
-    path('verify/<int:pk>/', verify, name='verify'),
-    path('accept_auth_log/<int:pk>', accept_auth_log, name='accept_auth_log'),
-    path('refuse_auth_log/<int:pk>', refuse_auth_log, name='refuse_auth_log'),
-    path('show_log/<int:pk>', show_log, name='show_log'),
+    path('activate/<int:room_id>/', activate, name='activate'),
+    path('authentication/<int:room_id>/<int:auth_id>', create_authentication, name='create_authentication'),
+    path('auth/<int:room_id>', create_auth, name='create_auth'),
+    path('verify/<int:room_id>/', verify, name='verify'),
+    path('accept_auth_log/<int:member_auth_id>', accept_auth_log, name='accept_auth_log'),
+    path('refuse_auth_log/<int:member_auth_id>', refuse_auth_log, name='refuse_auth_log'),
+    path('show_log/<int:room_id>', show_log, name='show_log'),
     path('member_list/<int:room_id>', show_member_list, name="member_list"),
     path('permission_check', permission_check, name='permission_check'),
     path('withdraw_from_room', withdraw_from_room, name='withdraw_from_room'),
+    path('close_auth/<int:room_id>/<int:auth_id>', close_authentication, name='close_auth'),
 ]
