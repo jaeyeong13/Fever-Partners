821a1857d0ee44292d984c6caec3a295cfa4dabe
hbam1 <hbbaamm@gmail.com>
2024-02-15 23:27:46 +0900

    [feat] login~main css 작업 1차

--- apps/user_management/forms.py
+++ apps/user_management/forms.py
@@ -3,12 +3,42 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User
 
 class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(
+        max_length=254,
+        help_text='Enter your email',
+        widget=forms.EmailInput(attrs={'placeholder': '이메일 입력'}))
+    
+    password1 = forms.CharField(
+        max_length=30, required=True, widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "비밀번호 입력"})
+    )
+
+    password2 = forms.CharField(
+        max_length=30, required=True, widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "비밀번호 확인"})
+    )
+
     class Meta:
         model = User
-        fields = ["email"]
+        fields = ["email", "password1", "password2"]
 
 
 class UserUpdateForm(UserChangeForm):
+    nickname = forms.CharField(
+        max_length=30,
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': '닉네임 입력'}))
+    profile = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': '소개글 입력'}))
+    region = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': '거주지역 입력'}))
+    region_detail = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': '거주지역 세부 입력'}))
+
     class Meta:
         model = User
         fields = ["nickname", "profile", "profile_image", "region", "region_detail"]
@@ -16,8 +46,8 @@ class UserUpdateForm(UserChangeForm):
 
 class LoginForm(forms.Form):
     email = forms.CharField(
-        max_length=100, required=True, widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "이메일"})
+        max_length=100, required=True, widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "이메일 입력"})
     )
     password = forms.CharField(
-        max_length=30, required=True, widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "패스워드"})
+        max_length=30, required=True, widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "비밀번호 입력"})
     )
