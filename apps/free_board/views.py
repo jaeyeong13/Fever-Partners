ebd38d52ddfd0012715f47cab7a7c6d218c11114
Jaeyeong Won <devlog.jae13@gmail.com>
2024-02-16 14:58:46 +0900

    [feat] '목록' 버튼 눌렀을 때 유저가 직전에 있었던 탭(공지/자유)으로 되돌아감

--- apps/free_board/views.py
+++ apps/free_board/views.py
@@ -61,22 +61,22 @@ def create_comment(request, study_room_id, post_id):
 
 def index(request, study_room_id):
     room = get_object_or_404(Room, pk=study_room_id)
-    tab = request.GET.get('tab', 'notice')
-
-    notice_posts = Post.objects.filter(notice=True, room=room).order_by('-created_at')[:2]  # 여기를 수정해주세요.
-    if tab == 'notice':
-        posts = Post.objects.filter(notice=True, room=room).order_by('-created_at')  # 여기를 수정해주세요.
+    tab = request.GET.get('tab')
+    if tab is not None:
+        request.session['tab'] = tab
     else:
-        free_posts = Post.objects.filter(notice=False, room=room).order_by('-created_at')  # 여기를 수정해주세요.
+        tab = request.session.get('tab', 'notice')
+    notice_posts = Post.objects.filter(notice=True, room=room).order_by('-created_at')[:2]
+    if tab == 'notice':
+        posts = Post.objects.filter(notice=True, room=room).order_by('-created_at')
+    else:
+        free_posts = Post.objects.filter(notice=False, room=room).order_by('-created_at')
         posts = list(chain(notice_posts, free_posts))
-
-    paginator = Paginator(posts, 8)
+    paginator = Paginator(posts, 7)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-
     context = {'page_obj': page_obj, 'tab': tab, 'room_id': study_room_id, 'room': room}
     return render(request, 'free_board/board_list.html', context)
-
 
 
 
